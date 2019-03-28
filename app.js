7d460f05f6b23488a29d921d8f143719ea6c5096
EC2 Default User <ec2-user@ip-172-31-39-251.eu-central-1.compute.internal>
2019-03-28 20:58:03 +0000

    count store into redis

--- app.js
+++ app.js
@@ -1,17 +1,17 @@
 var http = require('http');
-var userCount = 0;
-var server = http.createServer(function (require, response) {
-    if (require.url === '/favicon.ico'){
-        console.log('favicon');
-        return;
-    }
-    userCount++;
-    response.writeHead(200, { 'Content-Type': 'text/plain' });
-    response.write('Welcome !\n');
-    response.write('We were visited ' + userCount + ' times!\n');
-    response.end();
+var redis = require('redis').createClient();
+   
+   http.createServer(function (require, response) {
+       if (require.url === '/favicon.ico'){
+//           console.log('favicon');
+           return;
+        }
+    redis.incr("count",function(err, reply) {
+      response.writeHead(200, { 'Content-Type': 'text/plain' });
+      response.write('Welcome !\n');
+      response.write('We were visited ' + reply.toString() + ' times!\n');
+      response.end();
+     });
 
-});
-
-server.listen(9090);
+  }).listen(9090);
 console.log('server is running')
