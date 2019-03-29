7198a3df7968e2c07227968cea66ad440572bb0f
dasha <daswko@gmail.com>
2019-03-29 14:04:02 +0000

    update script

--- run_task1.sh
+++ run_task1.sh
@@ -3,6 +3,7 @@ sudo yum install docker -y
 sudo service docker start
 echo "docker application is installed and started"
 
+sudo docker network create redis
 sudo docker build -t my-nodejs-app .
 sudo docker run -d  --net "redis" --name redis-server -p 6379:6379 redis
 sudo docker run -d --name my-running-app --net "redis" -p 9090:9090 --link redis-server:redis my-nodejs-app
