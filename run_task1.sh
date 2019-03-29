1cf5cbced0d3fa8477beb96ab27061c62af34c93
dasha <daswko@gmail.com>
2019-03-29 14:20:44 +0000

    update script. how to check redis

--- run_task1.sh
+++ run_task1.sh
@@ -9,4 +9,5 @@ sudo docker run -d  --net "redis" --name redis-server -p 6379:6379 redis
 sudo docker run -d --name my-running-app --net "redis" -p 9090:9090 --link redis-server:redis my-nodejs-app
 
 echo "check web app in browser yourIP:9090"
-echo "check redis use cli redis-cli. type get count"
+echo "to check redis run  sudo docker exec -it redis-server /bin/bash"
+echo "get count"
