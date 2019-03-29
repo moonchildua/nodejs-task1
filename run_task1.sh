c01a46a401d0d892bee053c6ec40752ba0b56389
dasha <daswko@gmail.com>
2019-03-29 15:41:35 +0000

    update script. install locally redis-cli

--- run_task1.sh
+++ run_task1.sh
@@ -1,8 +1,24 @@
 #!/bin/bash
+sudo yum install gcc make -y
+cd ..
+wget http://download.redis.io/redis-stable.tar.gz
+tar xvzf redis-stable.tar.gz
+cd redis-stable
+make
+sudo make /etc/redis
+sudo mkdir /etc/redis
+sudo mkdir /var/redis
+sudo cp utils/redis_init_script /etc/init.d/redis_6379
+sudo cp redis.conf /etc/redis/6379.conf
+sudo mkdir /var/redis/6379
+sudo cp src/redis-cli /usr/local/bin
+echo "redis-cli is installen on local machine"
+
+
 sudo yum install docker -y
 sudo service docker start
 echo "docker application is installed and started"
-
+cd ../nodejs-task1/
 sudo docker network create redis
 sudo docker build -t my-nodejs-app .
 sudo docker run -d  --net "redis" --name redis-server -p 6379:6379 redis
